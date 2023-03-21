323b437b0d837a3e8ee60965515e7c95ada45211
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-03-21 15:42:45 +0900

    Add LogoView Detail

--- tip-calculator/view/LogoView.swift
+++ tip-calculator/view/LogoView.swift
@@ -9,6 +9,51 @@ import UIKit
 
 class LogoView: UIView {
     
+    private let imageView: UIImageView = {
+        let view = UIImageView(image: .init(named: "icCalculatorBW"))
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    private let topLabel: UILabel = {
+        let label = UILabel()
+        let text = NSMutableAttributedString(
+            string: "Mr TIP",
+            attributes: [.font: ThemeFont.dembold(ofSize: 16)]
+        )
+        text.addAttributes([.font: ThemeFont.bold(ofSize: 24)], range: NSMakeRange(3, 3))
+        label.attributedText = text
+        return label
+    }()
+    
+    private let bottomLabel: UILabel = {
+        LabelFactory.build(
+            text: "Calculator",
+            font: ThemeFont.dembold(ofSize: 20),
+            textAlignment: .left)
+    }()
+    
+    private lazy var vStackViews: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [
+            topLabel,
+            bottomLabel
+        ])
+        view.axis = .vertical
+        view.spacing = -4
+        return view
+    }()
+    
+    private lazy var hStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [
+            imageView,
+            vStackViews
+        ])
+        view.axis = .horizontal
+        view.spacing = 8
+        view.alignment = .center
+        return view
+    }()
+    
     init() {
         super.init(frame: .zero)
         layout()
@@ -19,6 +64,14 @@ class LogoView: UIView {
     }
     
     private func layout() {
-        backgroundColor = .red
+        addSubview(hStackView)
+        hStackView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.centerX.equalToSuperview()
+        }
+        
+        imageView.snp.makeConstraints { make in
+            make.height.equalTo(imageView.snp.width)
+        }
     }
 }
