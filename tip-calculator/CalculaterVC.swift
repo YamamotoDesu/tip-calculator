f2aa258374b199ba5f44ffe2af02751ac440cec8
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-03-31 16:03:56 +0900

    Display Result In Result View

--- tip-calculator/CalculaterVC.swift
+++ tip-calculator/CalculaterVC.swift
@@ -49,8 +49,8 @@ class CalculaterVC: UIViewController {
         
         let output = vm.transform(input: input)
 
-        output.updateViewPublisher.sink { result in
-            print(result)
+        output.updateViewPublisher.sink { [unowned self] result in
+            resultView.configure(result: result)
         }.store(in: &cancellables)
     }
 
